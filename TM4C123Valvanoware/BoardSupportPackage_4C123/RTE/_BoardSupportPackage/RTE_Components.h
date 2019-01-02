e8c83de8bc52b1bae47f899b5d1bfbe0a0f3fd44
thechozenwan <turynlb@gmail.com>
2019-01-02 18:10:12 +0200

    Ran 3  projects

--- TM4C123Valvanoware/BoardSupportPackage_4C123/RTE/_BoardSupportPackage/RTE_Components.h
+++ TM4C123Valvanoware/BoardSupportPackage_4C123/RTE/_BoardSupportPackage/RTE_Components.h
@@ -3,12 +3,18 @@
  * Auto generated Run-Time-Environment Component Configuration File
  *      *** Do not modify ! ***
  *
- * Project: 'Lab1' 
- * Target:  'Target' 
+ * Project: 'BoardSupportPackage' 
+ * Target:  'BoardSupportPackage' 
  */
 
 #ifndef RTE_COMPONENTS_H
 #define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "TM4C123.h"
 
 
 #endif /* RTE_COMPONENTS_H */
